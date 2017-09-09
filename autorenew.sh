9d5834cbb446a3ba299a39a9ce707d034b04f921
Tech Blog <31671197+Tech-Blog@users.noreply.github.com>
2017-09-10 01:39:04 +0300

    Update autorenew.sh

--- autorenew.sh
+++ autorenew.sh
@@ -1,5 +1,2 @@
 #echo new cron into cron file
-echo "15 3 * * * /usr/bin/certbot renew --quiet" >> cerbot
-#install new cron file
-crontab certbot
-rm certbot
+(crontab -l && echo "15 3 * * * /usr/bin/certbot renew --quiet") | crontab
